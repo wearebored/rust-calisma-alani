3c8914b361512126dd3ebbaa009734b3f9813566
enes <enesooo78@gmail.com>
2022-11-25 16:40:25 +0300

    rust

--- tahmin_oyunu/src/main.rs
+++ tahmin_oyunu/src/main.rs
@@ -1,29 +1,52 @@
-use std::io;
-use rand::Rng;
-use std::cmp::Ordering;
-fn main() {
-    println!("Sayıyı tahmin edin! ");
-    let rasgele =rand::thread_rng().gen_range(1..=100);
-    // println!("rasgele sayı : {rasgele}");
-    loop{
-    println!("Bir sayı giriniz:");
-    let mut sayı = String::new();
-    io::stdin().read_line(&mut sayı).expect("failed to read line");
-    let sayı: u32= match sayı.trim().parse(){
-        Ok(num)=>num,
+// use std::io;
+// use rand::Rng;
+// use std::cmp::Ordering;
+// fn main() {
+//     println!("Sayıyı tahmin edin! ");
+//     let rasgele =rand::thread_rng().gen_range(1..=100);
+//     // println!("rasgele sayı : {rasgele}");
+//     loop{
+//     println!("Bir sayı giriniz:");
+//     let mut sayı = String::new();
+//     io::stdin().read_line(&mut sayı).expect("failed to read line");
+//     let sayı: u32= match sayı.trim().parse(){
+//         Ok(num)=>num,
       
-        Err(_)=>continue,
-    };
-    println!("sectiğin sayı: {sayı}");
-    match sayı.cmp (&rasgele){
-        Ordering::Less=>println!("sayı küçük"),
-        Ordering::Greater=> println!("sayı büyük"),
-        Ordering::Equal=>{println!("kazandın");
-    break;
-    }
+//         Err(_)=>continue,
+//     };
+//     println!("sectiğin sayı: {sayı}");
+//     match sayı.cmp (&rasgele){
+//         Ordering::Less=>println!("sayı küçük"),
+//         Ordering::Greater=> println!("sayı büyük"),
+//         Ordering::Equal=>{println!("kazandın");
+//     break;
+//     }
     
-    }
-}
+//     }
+// }
 
 
+// }
+
+// fn main(){
+//     let mut x = 5;
+//     println!("x in değeri {x} tir");
+//     x=22;
+//     println!("x in yeni değeri {x} tir");
+// }
+// fn main(){
+//     let x = 5;
+//     let x =x+10;
+//     {
+//         let x =x *2;
+//         println!{"x in son değeri {x} dir"}
+
+//     }
+//     println!{"x in değeri{x}"}
+// }
+
+fn main(){
+    let boşluk = "      ";
+    let boşluk = boşluk.len();
+    println!("boşluk sayısı {boşluk}")
 }
